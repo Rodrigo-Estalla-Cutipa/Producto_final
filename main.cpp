9daa2519a41d8d4dc5a417bf3fa5bd918a92d343
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-07-12 17:44:03 -0500

    prototipo de archivo main

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct registro{
+    f
+    //usuario, contraseña, email, numero telf,
+};
+
+struct cliente{
+    f
+    //usuario o email, contraseña.
+};
+
+struct proveedor{
+    //usuario o email, contraseña.
+};
+
+struct direc{
+    f
+    //departament, prov, distrito, mz,lt, piso(opcional)
+};
+
+struct pago{
+    float
+    //pago_efectivo, yape, cupón descuento, tarjeta debito y crédito.
+};
+
+struct producto{
+    float precio;
+    int cantidad;
+    //agregar mas elementos
+};
