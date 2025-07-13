6b937c27e96734567c572926e3024d8a4291f0cd
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-07-12 20:13:08 -0500

    Actualiza archivo main.cpp : prototipo del menu de inicio

--- main.cpp
+++ main.cpp
@@ -50,3 +50,44 @@ struct comentario {
     string titulo;
     string mensaje;
 };
+
+int main() {
+    int op;
+    char salir;
+
+    do {
+        system("cls");
+        cout << "==================================" << endl;
+        cout << "       BIENVENIDO A PidePE " << endl;
+        cout << "==================================" << "\n" << endl;
+        cout << "1. Iniciar sesion" << endl;
+        cout << "2. Registrarse" << endl;
+        cout << "0. Salir" << endl;
+        cout << "----------------------------------" << endl;
+        cout << "Elige una opcion: "; cin >> op;
+        cout << endl;
+
+        switch(op) {
+            case 1:
+                cout << "Iniciandose" << endl; // Aqui haremos inicio de sesion
+                system("pause");
+                break;
+            case 2:
+                cout << ">> Registrandose" << endl; // Aqui habra una funcion de registro
+                system("pause");
+                break;
+            case 0:
+                cout << "Esta seguro de salir? (S/n): "; cin >> salir;
+                if (salir == 'N' || salir == 'n') {
+                    op = -1;
+                }
+                break;
+            default:
+                cout << "OPCION NO VALIDA" << endl;
+                system("pause");
+                break;
+        }
+    } while(op != 0);
+
+    return 0;
+}
